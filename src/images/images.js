6fcac853ace76a56ceccf773503fae88b13a7e3d
Seth Kingsbury <swk22@uclive.ac.nz>
2023-01-16 14:40:58 +1300

    images updated

--- src/images/images.js
+++ src/images/images.js
@@ -1,61 +1,22 @@
-import img0 from './IMG_9180.JPG'
-import img1 from './IMG_9181.JPG'
-import img2 from './IMG_9182.JPG'
-import img3 from './IMG_9183.JPG'
-import img4 from './IMG_9184.JPG'
-import img5 from './IMG_9185.JPG'
-import img6 from './IMG_9186.JPG'
-import img7 from './IMG_9187.JPG'
-import img8 from './IMG_9188.JPG'
-import img9 from './IMG_9189.JPG'
-import img10 from './IMG_9190.JPG'
-
-
 const ItemData = [
     {
-      img: img0,
-      title: 'Bed',
+      img: require('./IMG_0000.jpeg'),
+      title: '0',
     },
-    {
-      img: img1,
-      title: 'Books',
-    },
-    {
-      img: img2,
-      title: 'Sink',
-    },
-    {
-      img: img3,
-      title: 'Kitchen',
-    },
-    {
-      img: img4,
-      title: 'Blinds',
-    },
-    {
-      img: img5,
-      title: 'Blinds',
-    },
-    {
-      img: img6,
-      title: 'Blinds',
-    },
-    {
-      img: img7,
-      title: 'Blinds',
-    },
-    {
-      img: img8,
-      title: 'Blinds',
-    },
-    {
-      img: img9,
-      title: 'Blinds',
-    },
-    {
-      img: img10,
-      title: 'Blinds',
-    }
   ];
+
+for (let i = 0; i < 10; i++) {
+  ItemData.push({
+    img: require(`./IMG_000${i}.jpeg`),
+    title: i.toString(),
+  })
+}
+
+for (let i = 10; i < 51; i++) {
+  ItemData.push({
+    img: require(`./IMG_00${i}.jpeg`),
+    title: i.toString(),
+  })
+}
 
   export default ItemData
