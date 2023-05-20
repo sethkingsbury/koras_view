955be49e7d8ec5d46920f6ef470d71503b4ef18b
Seth Kingsbury <swk22@uclive.ac.nz>
2023-05-20 14:54:04 +1200

    added styling for the reviews page

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,7 +2,10 @@ import MasonryImageList from "../components/MasonryImageList"
 
 function Gallery({ isMobile }) {
     return (
-        <div className="container">
+        <div>
+            <div className="page-title">
+                Gallery
+            </div>
             <div className="gallery">
                 <MasonryImageList isMobile={isMobile}/>
             </div>
--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -3,15 +3,17 @@ import reviews from '../mockData/reviews'
 
 function Reviews () {
     return (
-        <div className="container">
+        <div>
             <div className="page-title">
                 Reviews
             </div>
-            <Review review={reviews[0]}/>
-            <Review review={reviews[1]}/>
-            <Review review={reviews[2]}/>
-            <Review review={reviews[3]}/>
-            <Review review={reviews[4]}/>
+            <div className="review-container">
+                <Review review={reviews[0]}/>
+                <Review review={reviews[1]}/>
+                <Review review={reviews[2]}/>
+                <Review review={reviews[3]}/>
+                <Review review={reviews[4]}/>
+            </div>
         </div>
     )
 }
