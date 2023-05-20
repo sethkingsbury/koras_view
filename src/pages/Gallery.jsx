495d4007cc4f033ed4c1f1618b300ff6cf76e33a
Seth Kingsbury <swk22@uclive.ac.nz>
2023-05-20 14:42:25 +1200

    updated header stlyes so game to route container always the same

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -3,7 +3,6 @@ import MasonryImageList from "../components/MasonryImageList"
 function Gallery({ isMobile }) {
     return (
         <div className="container">
-            <div className="page-title">Gallery</div>
             <div className="gallery">
                 <MasonryImageList isMobile={isMobile}/>
             </div>
